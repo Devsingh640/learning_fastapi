02cfd5f3f71fe9d3551dad2510c60d76faf5f9d1
devsingh640 <devsingh640@gmail.com>
2025-10-04 20:36:35 +0530

    code cleaning

--- main.py
+++ main.py
@@ -18,22 +18,6 @@ class StudentData(BaseModel):
 
 students=[] # list of objects (dictionaries)
 
-
-student_data = {
-    "1234":     {"student_roll_no": "1234",
-                "student_name": "raja",
-                "student_class" : "s3",
-                "student_contact": "1234567890"},
-
-    "4321": {"student_roll_no": "4321",
-             "student_name": "sumit",
-             "student_class": "s2",
-             "student_contact": "0987654321"}
-}
-
-
-
-
 @app.get("/")
 def index():
     return "hello world"
@@ -55,17 +39,6 @@ def fetch_students():
         # error is here
         return students
 
-"""
-@app.get("/fetch-students", response_model=list(StudentData))
-def fetch_students():
-    if len(students) <=0:
-        return {"message": "data not found"}
-    else:
-        # error is here
-        return students
-
-"""
-
 
 @app.get("/fetch-student-by-roll-no/{rn}")
 def fetch_student_by_roll_no(rn: int, response_model = StudentData):
@@ -79,25 +52,25 @@ def fetch_student_by_roll_no(rn: int, response_model = StudentData):
         return students[rn]
 
 
-# UPDATE
-@app.put("/update-student/{rn}")
-def update_student(rn):
-    print(rn)
-
-    updated_dict = {"student_roll_no": rn,
-                "student_name": "vijay",
-                "student_class" : "s6",
-                "student_contact": "8765432345"}
-
-    student_data.update({rn:updated_dict})
+# # UPDATE
+# @app.put("/update-student/{rn}")
+# def update_student(rn):
+#     print(rn)
+#
+#     updated_dict = {"student_roll_no": rn,
+#                 "student_name": "vijay",
+#                 "student_class" : "s6",
+#                 "student_contact": "8765432345"}
+#
+#     student_data.update({rn:updated_dict})
 
     return f"student with roll no. {rn} updated"
 
-@app.delete("/delete-student/{rn}")
-def delete_student(rn):
-    print(rn)
-    student_data.pop(str(rn))
-    return f"student with roll no. {rn} deleted"
+# @app.delete("/delete-student/{rn}")
+# def delete_student(rn):
+#     print(rn)
+#     student_data.pop(str(rn))
+#     return f"student with roll no. {rn} deleted"
 
 
 
