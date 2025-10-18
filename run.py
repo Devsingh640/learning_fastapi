7610ddcb92820b0561aba765a58a0fa16691791b
devsingh640 <devsingh640@gmail.com>
2025-10-18 20:10:01 +0530

    docker file and deployment.sh created

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import uvicorn
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
+    host = "0.0.0.0"
+    """
+    runs the application both on localhost and system ip
+    """
     port = 8000
     reload = True
 
