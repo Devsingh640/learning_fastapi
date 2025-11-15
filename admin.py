0b710f2324e65af636fbf2ed39a47d169b734c8a
devsingh640 <devsingh640@gmail.com>
2025-11-15 20:51:33 +0530

    TODO and User admin view addded

--- admin.py
+++ admin.py
@@ -3,7 +3,8 @@ from starlette.staticfiles import StaticFiles
 from starlette_admin.contrib.sqlmodel import Admin, ModelView
 from src.packages.utilities.pos_db import engine
 from src.packages.students.model import Student
-
+from src.packages.todo.model import Todo
+from src.packages.user.model import User
 
 
 admin = Admin(engine,
@@ -13,10 +14,18 @@ admin = Admin(engine,
 class StudentView(ModelView):
     pass
 
+class TodoView(ModelView):
+    pass
+
+class UserView(ModelView):
+    pass
+
 def init_admin(app: FastAPI):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
     admin.add_view(StudentView(Student, label="Students"))
+    admin.add_view(TodoView(Todo, label="Todo"))
+    admin.add_view(UserView(User, label="User"))
 
     admin.mount_to(app)
 
