5626bf93cc1197c80d80d816796e4ecbdbb6cf4c
devsingh640 <devsingh640@gmail.com>
2025-11-30 19:43:45 +0530

    scheduler example

--- testing_scheduler.py
+++ testing_scheduler.py
@@ -1,29 +1,33 @@
-from time import sleep
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 import asyncio
 
 async def job_first():
-    print("Started executing task in background")
-    await asyncio.sleep(5)
-    print("Task executing completed in background")
+    print("First Started")
+    await asyncio.sleep(1)
+    print("First Stopped")
 
-async def my_daily_task(i):
-    print(f"I performed my todays task {i}")
+async def job_second():
+    print("Second Started")
     await asyncio.sleep(5)
+    print("Second Stopped")
+
+async def wait_function(i):
+    await asyncio.sleep(i)
 
 async def main():
     scheduler = AsyncIOScheduler()
     try:
-        scheduler.add_job(job_first, IntervalTrigger(seconds=10), id="job_first")
+        scheduler.add_job(job_first, IntervalTrigger(seconds=2), id="job_first")
+        scheduler.add_job(job_second, IntervalTrigger(seconds=6), id="job_second")
         scheduler.start()
 
-        i = 1
+        print(scheduler.print_jobs())
+
         while True:
-            await my_daily_task(i)
-            i+=1
+            await wait_function(1)
+
     except Exception as error:
         print("Error: ", error)
     finally:
